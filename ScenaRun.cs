7d12ad1892a40a1c4810f1f16bf179f339900162
Tonorraptor <121475061+Tonorraptor@users.noreply.github.com>
2026-04-18 12:46:57 -0500

    Add finalScenas

---
+++ ScenaRun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase4Ejercicio1
+{
+    internal class ScenaRun
+    {
+        public static void Run()
+        {
+            Console.WriteLine("ESCENA 3: LA HUIDA");
+            Console.WriteLine("El khan lo atrapa");
+            Console.WriteLine("Mustafa muere");
+        }
+    }
+}
