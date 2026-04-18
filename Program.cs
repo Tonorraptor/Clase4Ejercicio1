dda47e85efc0efa00d4c8400e9672802d914ec68
Tonorraptor <121475061+Tonorraptor@users.noreply.github.com>
2026-04-18 12:05:14 -0500

    Inicialization

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clase4Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
